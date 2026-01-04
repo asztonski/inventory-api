8b3815ba3ff222848956f0fe065c179536ee6c60
asztonski <asztonski@gmail.com>
2026-01-04 19:47:20 +0100

    Add typescript

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
-const express = require("express");
-const app = express();
+import express from "express";
 
+const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
